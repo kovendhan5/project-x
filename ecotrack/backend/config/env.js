dfc5198acd39f94a0b2fb1abde5ef2e2e7c87b77
kovendhan5 <kovendhanofficial5@gmail.com>
2025-02-04 21:48:04 +0530

    react - build

--- ecotrack/backend/config/env.js
+++ ecotrack/backend/config/env.js
@@ -1,5 +1,5 @@
 // .env file (backend/.env)
-MONGODB_URI = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/EcoTrack?retryWrites=true&w=majority"
-JWT_SECRET = "your_jwt_secret"
-NEWS_API_KEY = "your_newsapi_key"
+MONGODB_URI = "mongodb://mongodb:27017/ecotrack"
+JWT_SECRET = "openssl rand -hex 32"
+NEWS_API_KEY = "31d0ca7878134c24b97dd6db9a4496a0"
 OPENAQ_API_KEY = "402e110815f617a7a4a96f27b104c93615116e64300a1b450fca4bccea1d7e0d"
