3a2a6d3fbf48b11ddfcc1e73682ad847bf0c1786
kovendhan5 <kovendhanofficial5@gmail.com>
2025-02-04 21:56:37 +0530

    Add HomePage component and update index.js for rendering

--- ecotrack/frontend/src/index.js
+++ ecotrack/frontend/src/index.js
@@ -1,1 +1,12 @@
+import { CssBaseline } from '@mui/material';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Import the App component
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CssBaseline />
+    <App />
+  </React.StrictMode>
+);
