7692ae2ef8cfb514c45a5f4be08d507cd52e4b38
kovendhan5 <kovendhanofficial5@gmail.com>
2025-02-04 19:56:59 +0530

    Add initial backend dependencies and configuration files

--- ecotrack/backend/config/env.js
+++ ecotrack/backend/config/env.js
@@ -1,5 +1,5 @@
 // .env file (backend/.env)
-MONGODB_URI = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/ecotrack?retryWrites=true&w=majority"
+MONGODB_URI = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/EcoTrack?retryWrites=true&w=majority"
 JWT_SECRET = "your_jwt_secret"
 NEWS_API_KEY = "your_newsapi_key"
 OPENAQ_API_KEY = "402e110815f617a7a4a96f27b104c93615116e64300a1b450fca4bccea1d7e0d"
