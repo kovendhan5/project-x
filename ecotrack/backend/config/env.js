32746dc921e5658a16590494372ce8985f464b82
kovendhan5 <kovendhanofficial5@gmail.com>
2025-02-09 16:34:25 +0530

    Update MongoDB URI in backend configuration

--- ecotrack/backend/config/env.js
+++ ecotrack/backend/config/env.js
@@ -1,5 +1,5 @@
 // .env file (backend/.env)
-MONGODB_URI = "mongodb://mongodb:27017/ecotrack"
+MONGODB_URI="mongodb+srv://kovendhan2535:GSqqMvbHIYtaqqru@cluster0.tbgew.mongodb.net/Epicwallpaper?retryWrites=true&w=majority&appName=Cluster0"
 JWT_SECRET = "openssl rand -hex 32"
 NEWS_API_KEY = "31d0ca7878134c24b97dd6db9a4496a0"
 OPENAQ_API_KEY = "402e110815f617a7a4a96f27b104c93615116e64300a1b450fca4bccea1d7e0d"
